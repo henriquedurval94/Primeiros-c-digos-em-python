7447fdc141538610ce7a500785c1c6561966896c
Henrique Durval <hdurval17@gmail.com>
2026-04-09 13:52:50 -0300

    projeto_operadoresmatematicos.py
    Implement basic mathematical operations and display results.

---
+++ projeto_operadoesmatematicos.py
@@ -0,0 +1,24 @@
+# ENTRADA DE DADOS
+n1 = 15
+n2 = 2
+
+#PROCESSANDO OS DADOS E FAZENDO AS FUNÇÕES /  OPERADORES MATEMÁTICOS 
+adicao = n1 + n2
+sub = n1 - n2
+mult = n1 * n2
+div = n1 / n2
+rest_div = n1 % n2
+expoente = n1 ** n2
+floor_div = n1 // n2
+
+# SAIDA COM OS RESULTADOS DOS DADOS NA TELA. 
+print()
+print("O resultado é:", adicao)
+print("O resultado é:", sub)
+print("O resultado é:", mult)
+print("O resultado é:", div)
+print("O resultado é:", rest_div)
+print("O resultado é:", expoente)
+print("O resultado é:", floor_div)
+
+PROGRAMA RODANDO E TRASENDO A INFORMAÇÃO NA TELA PARA  O USUÁRIO. 
