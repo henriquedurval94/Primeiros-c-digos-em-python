206d69c8f2fbefa05c0313825e740f2926e588da
Henrique Durval <hdurval17@gmail.com>
2026-04-09 13:18:33 -0300

    projeto_calculotriplo.py

--- projeto_calculotriplo.py
+++ projeto_calculotriplo.py
@@ -1,6 +1,12 @@
 print("="*15, "CALCULO TRIPULO" ,"="*15)
+
+#Entrada de Dados.
 n1 = input("Digite um número: ")
+
+#Processamento de Dados
 triplo = int(n1) * 3
 
+
+#Saida de Dados
 print()
 print("O tripo de", n1,"é", triplo)
